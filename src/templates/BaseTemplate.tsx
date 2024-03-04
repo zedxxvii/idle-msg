9479e9b13da1ea44ad40698691cb53ab187c6e96
kai <hengsokchamrern@Mac.local>
2024-03-04 10:24:19 +0700

    locale changed

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -40,15 +40,15 @@ const BaseTemplate = (props: {
         <footer className="border-t border-gray-300 py-8 text-center text-sm">
           © Copyright {new Date().getFullYear()} {AppConfig.name}.
           {` ${t('made_with')} `}
-          {/* <a
+          <a
             href="https://creativedesignsguru.com"
             className="text-blue-700 hover:border-b-2 hover:border-blue-700"
           >
             CreativeDesignsGuru
-          </a> */}
-          <p className="text-blue-700 hover:border-b-2 hover:border-blue-700">
+          </a>
+          {/* <p className="text-blue-700 hover:border-b-2 hover:border-blue-700">
             Zed
-          </p>
+          </p> */}
           .
           {/*
            * PLEASE READ THIS SECTION
